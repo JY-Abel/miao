1b1efc4f7148e03e95d7f492aed070235e065bec
pan jiang yong <jiangyong@qq.com>
2022-09-14 22:05:26 +0800

    js

--- lodash/jy_abel-lodash.js
+++ lodash/jy_abel-lodash.js
@@ -20,6 +20,22 @@ var abel = {
     return filter
   },
 
-  fill: function() {},
+  fill: function(array,value,[start = 0], [end = array.length]) {
+    var newAry = [] // 新的数组
+    newAry = array.slice() // 复制数组
+    for (var i = start; i < end; i++) { // 遍历数组
+      newAry.shift()
+    }
+    return newAry
+  },
+
+  drop: function(array, n = 1) {
+     var result = []
+     result = array.slice()
+     for (var i = 1; i <= n; i++) {
+      result.shift()
+     }
+     return result
+  },
 
 }
